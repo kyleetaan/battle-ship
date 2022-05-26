0ed3700fb8b758a9ff6727c737bbb20ebc15cb4b
Kyle <tan.kyleemmanuel@gmail.com>
2022-05-26 22:21:54 +0800

    default ships now shown

--- src/modules/dom.js
+++ src/modules/dom.js
@@ -1,6 +1,6 @@
 import Player from "../factories/player";
 
-function fillBoard(boardArr, domTable, comp) {  // initial setup of board
+function fillBoard(boardArr, domTable) {  // initial setup of board
     domTable.textContent = '';
     for(let i = 0; i < boardArr.length; i++){
         for(let j = 0; j < boardArr[i].length; j++){
@@ -10,10 +10,6 @@ function fillBoard(boardArr, domTable, comp) {  // initial setup of board
             domTable.appendChild(cell);
             let cellClass = decideClass(boardArr[i][j]);
             cell.classList.add(cellClass);
-            if(!comp) {  // when not computer then its a table to add event listeners
-                // event listener here
-                // cell.addEventListener('click',)
-            }
         }                   
     }
 }
@@ -26,6 +22,35 @@ function decideClass(elemInArr){
     }
 }
 
-export {fillBoard}
+// function color() { 
+//     if(this.classList.contains("active_cell")){
+//         this.classList.remove("active_cell")
+//         this.classList.add('has_ship')
+//     } else {
+//         this.className = "cell active_cell"
+//     }
+// }
+
+// function refresh table to show ships
+function showShips(table, domTable){
+    // table.forEach(element => {
+    //     element.forEach(e => {
+    //         if(e != 'x'){
+
+    //         }
+    //     })
+    // });
+
+    for(let i = 0; i < table.length; i++){
+        for(let j = 0; j < table[i].length; j++){
+            if(table[i][j] != 'x'){
+                let ship = document.querySelector(`[data-coord='${i} ${j}']`);
+                ship.className = "cell has_ship";
+            }
+        }
+    }
+}
+
+export {fillBoard,showShips}
 
 // dom modules
--- src/modules/setupShip.js
+++ src/modules/setupShip.js
@@ -1,22 +1,95 @@
 import GameBoard from "../factories/gameboard";
-import { fillBoard } from "./dom";
+import Ship from "../factories/ship";
+import { fillBoard,showShips } from "./dom";
+
+const defaultCoords = { // for coords
+    "four": [[1,1], [1,2], [1,3], [1,4]],
+    "three1": [[1,7], [2,7], [3,7]],
+    "three2": [[6,4], [6,5], [6,6]],
+    "two1": [[3,4], [3,5]],
+    "two2": [[5,0], [6,0]],
+    "two3": [[9,4], [9,5]],
+    "one1": [[3,0]],
+    "one2": [[4,9]],
+    "one3": [[7,8]],
+    "one4": [[9,1]]
+}
 
 function setup() {
     const setupTable = document.getElementById('setup');
-    const table = new GameBoard();
+    const jumbleButton = document.getElementById('randomize');
+    jumbleButton.addEventListener('click', shipJumble);
 
-    fillBoard(table.board, setupTable);
+    const humanTable = new GameBoard();
+
+    fillBoard(humanTable.board, setupTable);
+    fillTableWithShip(humanTable.board, setupTable);
+    // const shipCoordsHumanTable = 
 }
 
 function shipJumble() {
-
+    console.log("jumble!")
 }
 
 function ifShipPositionValid() {
 
 }
 
-function fillTableWithShip() {
+function fillTableWithShip(table, domTable) { // default places
+    //four - 1
+    //three - 2
+    //two - 3
+    //one - 4
+
+    
+
+    let four = new Ship(4);
+    let three1 = new Ship(3);
+    let three2 = new Ship(3);
+    let two1 = new Ship(2);
+    let two2 = new Ship(2);
+    let two3 = new Ship(2);
+    let one1 = new Ship(1);
+    let one2 = new Ship(1);
+    let one3 = new Ship(1);
+    let one4 = new Ship(1);
+
+    //assign ships to coords
+
+    for(let key in defaultCoords){
+        if(defaultCoords.hasOwnProperty(key)){
+            for(let x of defaultCoords[key]){
+                if(key === "four"){
+                    //coord
+                    // let fourShip = document.querySelector(`[data-coord='${x[0]} ${x[1]}']`); 
+                    table[x[0]][x[1]] = four;
+                } else if(key ==="three1") {
+                    table[x[0]][x[1]] = three1;
+                } else if(key ==="three2") {
+                    table[x[0]][x[1]] = three2;
+                } else if(key ==="two1") {
+                    table[x[0]][x[1]] = two1;
+                } else if(key ==="two2") {
+                    table[x[0]][x[1]] = two2;
+                } else if(key ==="two3") {
+                    table[x[0]][x[1]] = two3;
+                } else if(key ==="one1") {
+                    table[x[0]][x[1]] = one1;
+                } else if(key ==="one2") {
+                    table[x[0]][x[1]] = one2;
+                } else if(key ==="one3") {
+                    table[x[0]][x[1]] = one3;
+                } else if(key ==="one4") {
+                    table[x[0]][x[1]] = one4;
+                }
+            }
+        }
+    }
+    // import dom func where it refreshes table to show ships
+    showShips(table, domTable);
+}
+
+function startingShips() {
 
 }
 
